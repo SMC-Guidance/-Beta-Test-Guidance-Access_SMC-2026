6a66e3ae479d9f60320cf81e9bea161ef45b416e
SMC-Guidance <joseangelo.reyes20@gmail.com>
2026-09-14 16:18:46 +0800

    Commit

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* SMC Guidance Center - service worker (PWA app shell) */
 "use strict";
-var CACHE = 'smc-guidance-v6-20260914-0943';
+var CACHE = 'smc-guidance-v7-20260914-1430';
 var CORE = [
   './',
   './index.html',
